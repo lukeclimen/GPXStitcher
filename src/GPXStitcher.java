9727ed6877df16b7a5009ab6823ca1b247555f0e
Luke Climenhage <luke.climenhage@gmail.com>
2022-06-12 15:05:35 -0400

    Separated out into methods, working on copyContent

--- src/GPXStitcher.java
+++ src/GPXStitcher.java
@@ -1,18 +1,59 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class GPXStitcher {
 
+    String timestamp;
+
     public static void main(String args[])  {
         try {
-            FileReader file1 = new FileReader("./First_Part.gpx");
-            FileReader file2 = new FileReader("./First_Part.gpx");
+            FileReader file1 = new FileReader("./First_Part.gpx", StandardCharsets.UTF_8);
+            FileReader file2 = new FileReader("./Second_Part.gpx", StandardCharsets.UTF_8);
+
+            File finalFile = new File("./Final.gpx");
+            boolean successful = finalFile.createNewFile();
+            if (successful){
+                System.out.println("File has been created");
+                copyContent(file1, finalFile);
+            }
+            else
+                System.out.println("File already exists");
         }
         catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    private static void copyContent (FileReader file1,  File finalFile) {
+        String lineToCopy;
+        BufferedReader fileToRead = new BufferedReader(file1);
+
+        try {
+            FileWriter stitcher = new FileWriter(finalFile);
+
+            for (int i = 0; i < 7990; i++) {
+                lineToCopy = fileToRead.readLine();
+                stitcher.write(lineToCopy);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void fileStitcher (FileReader file1,  File finalFile) {
+        int lineToCopy;
+
+        try {
+            BufferedWriter stitcher = new BufferedWriter(new FileWriter(finalFile));
+            for (int i = 0; i < 7990; i++) {
+                lineToCopy = file1.read();
+                stitcher.write(lineToCopy);
+                stitcher.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
